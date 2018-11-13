f5196b71f4480b39f0274fcf7e96aec8168479e7
Pushkar Tandon <ptondon515@gmail.com>
2018-11-14 00:39:05 +0530

    Updates

---
+++ precache-manifest.46655f1c9ab1460573597c57ec657a00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8072002ad4d4a898cf20495c03db697a",
+    "url": "/fitproject/static/media/hardik.8072002a.jpg"
+  },
+  {
+    "revision": "753d9b3de47b90e631ac0236f8fedce4",
+    "url": "/fitproject/static/media/fit.753d9b3d.jpeg"
+  },
+  {
+    "revision": "0ed4aa0ea36bccffdbab7568442ba9db",
+    "url": "/fitproject/static/media/Push.0ed4aa0e.jpg"
+  },
+  {
+    "revision": "9a1d83de60a52648c126",
+    "url": "/fitproject/static/js/runtime~main.9a1d83de.js"
+  },
+  {
+    "revision": "38738bf556569de7ffcb",
+    "url": "/fitproject/static/js/main.38738bf5.chunk.js"
+  },
+  {
+    "revision": "1eb44b45812371730f3f",
+    "url": "/fitproject/static/js/1.1eb44b45.chunk.js"
+  },
+  {
+    "revision": "38738bf556569de7ffcb",
+    "url": "/fitproject/static/css/main.d21995e6.chunk.css"
+  },
+  {
+    "revision": "64b6037245739c1a7641c1424d0f41f4",
+    "url": "/fitproject/index.html"
+  }
+];
